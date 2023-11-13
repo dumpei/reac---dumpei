8a4c0558531dd399676ca2a76ea670dd6154f083
dumpei <139002280+dumpei@users.noreply.github.com>
2023-11-13 14:24:16 +0000

    new

--- basics/02_data.js
+++ basics/02_data.js
@@ -1,1 +1,12 @@
 "use strict"
+// console.log(Math.random())
+// const min =10
+// const max = 20
+
+// console.log(Math.random() * (max-min + 1))
+
+// date
+
+let date = new Date()
+
+console.log(date.toString())
---
+++ basics/obj.js
@@ -0,0 +1,10 @@
+// const obj = {
+//     name:'rohit',
+//     age:'18',
+//     learn: 'js'
+// }
+
+// const{learn: l} = obj
+
+// console.log(l)
+
